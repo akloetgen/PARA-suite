dc6599137b074071068cbea9db0e48c2c7d7325f
Andreas Kloetgen <andreas.kloetgen@uni-duesseldorf.de>
2015-02-20 13:36:03 +0100

    snp_database added. examples.sh now shows example execution of additional tools

--- bin/examples.sh
+++ bin/examples.sh
@@ -16,5 +16,11 @@ java -jar parma.jar map -q ../examples/simulation/testout_simulation.fastq -r ..
 # benchmark PARMA alignment on simulated PAR-CLIP reads
 java -jar parma.jar benchmark ../examples/mapping/testout_simulation_mapped.combined.bam ../examples/mapping/testout_simulation_mapped.combined.stats ../examples/simulation/testout_simulation.fastq
 
+# calculate error profile
+java -jar parma.jar error ../examples/mapping/testout_simulation_mapped.combined.bam ../examples/references/reference_chr1.fa 51 -q
 
+# combine genomic and transcriptomic mapping
+java -jar parma.jar comb ../examples/mapping/testout_simulation_mapped.PARMA-genomic.bam ../examples/mapping/testout_simulation_mapped.PARMA-transcriptomic.bam ../examples/mapping/testout_simulation_mapped.PARMA.combined.bam
 
+# clustering aligned PAR-CLIP reads to obtain RBP-bound regions
+java -jar parma.jar clust ../examples/mapping/testout_simulation_mapped.combined.bam ../examples/references/reference_chr1.fa ../examples/mapping/testout_simulation_mapped.combined.clusters ../examples/references/snp_db.vcf.gz 1
